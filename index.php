561e4cafa7194fc7375cc3aca23e8a49647675ab
SunnyZaman <sunnyzaman@live.com>
2020-03-12 21:38:29 -0400

    php version

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html> 
+<head>      
+     <title>How to detect browser using PHP</title>        
+    </head>  
+    <body>        
+        <h1> Display Cross Browsers Compatibility Issues line1</h1>
+                 <?php
+                           echo " Trying to detect Browser name... <br/>";
+                                     function brdetect(){
+                                                         $res = $_SERVER['HTTP_USER_AGENT'];
+                                                         echo $res . "<br/><br/>";
+                                                        if ( strpos ($res, "Chrome") == true)
+                                                            echo "Browser: Google Chrome";
+                                                         else if ( strpos ($res, "Firefox") == true)        
+                                                               echo "Browser: Firefox";           
+                                                         else if ( strpos ($res, "Trident") == true)    
+                                                              echo "Browser: Internet Explorer";                      
+                                                         else  echo "Browser: unkown"; 
+                                                  } 
+                      brdetect( );   
+                      ?> 
+            </body> 
+</html>
